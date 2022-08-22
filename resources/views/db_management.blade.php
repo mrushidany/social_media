5ccb6f5d5d42b8b444378eda1c64eefd29add4e5
Dany <mrushidany@gmail.com>
2022-08-22 14:42:58 +0300

    Db management buttons

--- resources/views/db_management.blade.php
+++ resources/views/db_management.blade.php
@@ -2,14 +2,9 @@
 
 
 @section('content')
-<div class="row">
-    <div class="col">
-        <div class="col-md-6">
-            <a href="" class="btn btn-sm btn-primary">Migrate</a>
-        </div>
-        <div class="col-md-6">
-            <a href="" class="btn btn-sm btn-secondary">Clear Cache</a>
-        </div>
-    </div>
+<div class="d-flex justify-content-center">
+    <a href="" class="btn btn-sm btn-primary mr-3">Migrate</a>
+    <a href="" class="btn btn-sm btn-info mr-3">Roolback</a>
+    <a href="" class="btn btn-sm btn-secondary">Clear Cache</a>
 </div>
 @endsection
