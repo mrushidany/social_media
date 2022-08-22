97e2bbb3c62f4fd06aef40ce8778e20f925ae594
Dany <mrushidany@gmail.com>
2022-08-22 14:51:17 +0300

    Db functions complete

--- app/Http/Controllers/DatabaseManagementController.php
+++ app/Http/Controllers/DatabaseManagementController.php
@@ -3,19 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseManagementController extends Controller
 {
 
     public function migrate(){
-
+        Artisan::call('migrate');
+        return redirect()->route('db_management');
     }
 
     public function rollback(){
-
+        Artisan::call('migrate:rollback');
+        return redirect()->route('db_management');
     }
 
     public function clear_cache(){
-
+        Artisan::call('cache:clear');
+        return redirect()->route('db_management');
     }
 }
