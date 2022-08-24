bbb511af4978bd66faa40ed56ac7192220a68c1d
Dany <mrushidany@gmail.com>
2022-08-24 15:06:00 +0300

    Added the font

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <nav class="navbar navbar-expand-lg bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/ncba_mpawa.png') }}" alt="NCBA Mpawa" />
+            </a>
 
+        </div>
+    </nav>
+</div>
 
 @endsection
