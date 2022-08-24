0bf0b4eb3159c3e3fa209d8a238b9e02ea47420f
Dany <mrushidany@gmail.com>
2022-08-24 14:33:22 +0300

    Main layout

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('landing_page');
+})->name('landing_page');
+
 
 Route::get('/home', function () {
     return view('home');
