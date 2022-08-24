91292ccdd2b0575c959254411f5777f3211fb531
Dany <mrushidany@gmail.com>
2022-08-24 09:47:24 +0300

    get hashtag button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,7 @@
 
 @section('content')
 <p>We are already authenticated</p>
+<div class="d-flex justify-content-center">
+    <a href="" class="btn btn-sm btn-primary mr-3">Get Hashtag #</a>
+</div>
 @endsection
