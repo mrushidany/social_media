3bd0520e13de0cfb4ec3745ca3d9ac33b1be6658
Dany <mrushidany@gmail.com>
2022-08-24 16:39:22 +0300

    Navbar prototype

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -1,15 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <nav class="navbar navbar-expand-lg bg-white">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('images/ncba_mpawa.png') }}" alt="NCBA Mpawa" />
-            </a>
-
+<div class="header">
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-lg-2 col-md-4 col-6">
+                <a class="logo" href="">
+                    <img src="{{ asset('images/ncba_mpawa.png') }}" alt="NCBA MPAWA">
+                </a>
+            </div>
+            <div class="col-lg-1 col-md-1 col-6">
+                <div style="text-align:right">
+                    <span class="menu"><i class="fa fa-bars"></i></span>
+                </div>
+            </div>
+            <div class="col-lg-9 col-md-7 col-sm-12">
+                <div class="navigations">
+                    <ul class="navs">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Campaign</a></li>
+                        <li><a href="">How to participate</a></li>
+                        <li><a href="">Testimonials</a></li>
+                        <li><a href="">Contestors</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </nav>
+    </div>
 </div>
+
 
 @endsection
