ce895643fcd7fae505fd09fdbb8d6b026a5a73a4
Dany <mrushidany@gmail.com>
2022-08-25 08:49:44 +0300

    Challenge section

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -30,18 +30,6 @@
 <!-- home section ends -->
 
  <!-- about section starts -->
-{{-- <section class="about" id="about">
-    <div class="image">
-        <img src="{{ asset('images/about_chuzi.png') }}" alt="Chuzi Limekubali">
-        <a><img src="{{ asset('images/play.png') }}" alt="Press n Play"></a>
-    </div>
-    <div class="content">
-        <h3>#Chuzilimekubali</h3>
-        <p>Lorem ipsum dolor sit amet, consec tetur adipiscing elit. At viverra lectus tincidunt sed duis cursus aliquam. Viverra auctor adipiscing.dolor sit amet, consec tetur adipiscing elit. At viverra lectus tincidunt sed duis cursus aliquam. Viverra auctor adipiscing.dolor sit amet, consec tetur adipiscing elit. At viverra lectus tincidunt sed duis cursus aliquam. Viverra auctor adipiscing.</p>
-        <p>At viverra lectus tincidunt sed duis cursus aliquam. Vive rra auctor adipiscing.consec tetur adipiscing elit. At vive rra lectus tincidunt sed duis cursus aliquam.a auctor adipiscing.</p>
-    </div>
-
-</section> --}}
 <div class="container mt-4 pt-4">
     <div class="row mt-4">
         <div class="col-lg-6 col-md-5">
@@ -86,8 +74,266 @@
             <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis varius tortor dolor eu. Adipiscing nisl at adipiscing ut.</p>
         </div>
     </div>
+    <div class="row mt-3">
+        <div class="col-md-4">
+
+        </div>
+    </div>
  </div>
 <!-- How to participate section ends -->
+
+<!-- Word from winners section starts -->
+<section class="winners">
+    <div class="container">
+       <div class="row">
+            <div class="col-md-6">
+                <h3 class="mb-4">Word from our <span style="color: #3AB3E5;">Winners</span></h3>
+                <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis varius tortor dolor eu. Adipiscing nisl at adipiscing ut.</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-4 lr">
+                <div class="focus" style="background-image: url('{{ asset('images/boda.png') }}')">
+                    <div class="inner-focus">
+                        <div class="focus-icon">
+                            <span style="color: #392B27;">
+                            <i class="fa fa-play play"></i>
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 lr">
+                <div class="focus" style="background-image: url('{{ asset('images/sokoni_1.png') }}')">
+                    <div class="inner-focus">
+                        <div class="focus-icon">
+                            <span style="color: #392B27;">
+                            <i class="fa fa-play play"></i>
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 lr">
+                <div class="focus" style="background-image: url('{{ asset('images/sokoni_2.png') }}')">
+                    <div class="inner-focus">
+                        <div class="focus-icon">
+                            <span style="color: #392B27;">
+                            <i class="fa fa-play play"></i>
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=""></div>
+        </div>
+        <div class="row mt-5 mb-5">
+            <div class="slide">
+                <img src="{{ asset('images/blue-fill.png') }}" >
+                <img src="{{ asset('images/fill.png') }}" >
+                <img src="{{ asset('images/fill.png') }}" >
+                <img src="{{ asset('images/fill.png') }}" >
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Word from winners section ends -->
+
+<!-- Chuzi Challenge section starts -->
+<div class="container pt-0 participate-section">
+    <div class="row">
+        <h2>Chuzi <span style="color: #3AB3E5">Challenge</span></h2>
+        <div class="col-md-6 mt-3 pt-4">
+            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis varius tortor dolor eu. Adipiscing nisl at adipiscing ut.</p>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/mary_av.png') }}"><span class="name">Mary Shirima</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/1.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/john_av.png') }}"><span class="name">John Kilaza</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/2.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/david_av.png') }}"><span class="name">David Jicho</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/3.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/david_av.png') }}"><span class="name">Zuberi Hassan</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/4.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/mary_av.png') }}"><span class="name">Diane Joseph</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/5.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/mary_av.png') }}"><span class="name">Aneth Majaliwa</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/6.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/mary_av.png') }}"><span class="name">Amina Zuberi</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/7.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/kev_av.png') }}"><span class="name">Kelvin Shirima</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/8.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/mary_av.png') }}"><span class="name">Sisti Ngosha</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/9.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/mary_av.png') }}"><span class="name">Nadia Massawe</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/10.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/david_av.png') }}"><span class="name">Ngosha Ngosha</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/11.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mt-3 social-card">
+                <div class="card-body">
+                    <div class="heading">
+                        <img src="{{ asset('images/john_av.png') }}"><span class="name">Meku Muha</span>
+                    </div>
+                    <div class="body">
+                        <img src="{{ asset('images/challenge/2.png') }}" >
+                    </div>
+                    <div class="likes">
+                        <i class="fa-solid fa-heart"></i><span style="margin-left: 0.8rem">5.4k</span>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<!-- Chuzi Challenge section ends -->
+
 
 <!-- footer section starts -->
 <section class="footer">
