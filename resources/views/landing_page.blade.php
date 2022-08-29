80af01a1e26091659827e8a9f96f758c6587e242
Dany <mrushidany@gmail.com>
2022-08-29 14:47:39 +0300

    Removed the hero section

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -22,15 +22,9 @@
 <!-- home section starts -->
 <div class="home-bg">
     <section class="home" id="nyumbani">
-        <div class="hero">
-            <ul id="all_slides">
-                <li class="slide active"></li>
-                <li class="slide"></li>
-            </ul>
-            <div class="content">
-                <img src="{{ asset('images/app/chuzi_limekubali.png') }}" alt="Chuzi Limekubali" class="bounce">
-                <p class="mt-3">Shinda dabo ya akiba ukisevu na <span class="red">M-Pawa</span></p>
-            </div>
+        <div class="content">
+            <img src="{{ asset('images/app/chuzi_limekubali.png') }}" alt="Chuzi Limekubali" class="bounce">
+            <p class="mt-3">Shinda dabo ya akiba ukisevu na <span class="red">M-Pawa</span></p>
         </div>
     </section>
 </div>
